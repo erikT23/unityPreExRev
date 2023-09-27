de8e52e85a006812c8c656d969b34a4d6821d7c3
Erik Tapia <20203tn152@utez.edu.mx>
2023-09-27 17:33:04 -0600

    random

--- Assets/Scripts/CubeHops.cs
+++ Assets/Scripts/CubeHops.cs
@@ -1,13 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CubeHops : MonoBehaviour
 {
+
      [SerializeField] private Rigidbody rbComponent;
     [SerializeField] private float jumpForce;
     [SerializeField] private float time;
     private float currentTime;
+    private int a = UnityEngine.Random.Range(0, 2);
+    private bool Boolean;
 
     // Start is called before the first frame update
     void Start()
@@ -20,10 +25,37 @@ public class CubeHops : MonoBehaviour
     {
         currentTime -= Time.deltaTime;
 
+        
+        if (a == 0) 
+            Boolean = false;
+        else       
+        Boolean = true;
+
+        //if (currentTime <= 0)
+        //{
+        //    rbComponent.AddForce(jumpForce, jumpForce, 0f);
+        //    rbComponent.AddTorque(0f, 0f, -jumpForce * 0.5f);
+        //    currentTime = time;
+        //}
+
+
+
+
+
+        currentTime -= Time.deltaTime;
+
         if (currentTime <= 0)
         {
-            rbComponent.AddForce(jumpForce, jumpForce, 0f);
-            rbComponent.AddTorque(0f, 0f, -jumpForce * 0.5f);
+            if (Boolean)
+            {
+                rbComponent.AddForce(jumpForce, jumpForce, 0f);
+            }
+            else
+            {
+                rbComponent.AddForce(-jumpForce, jumpForce, 0f);
+            }
+            
+           
             currentTime = time;
         }
     }
